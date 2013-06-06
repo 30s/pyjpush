028fc361b00397811b4e75e1464b5429e9df089e
ax003d <ax003d@gmail.com>
2013-06-06 11:50:51 +0800

    * define API

---
+++ pyjpush/jpush.py
@@ -0,0 +1,13 @@
+class JPushClient(object):
+    def __init__(self):
+        pass
+
+    def sendmsg(self):
+        pass
+
+    def notification(self, sendno, app_key, receiver_type, receiver_value, 
+                     txt, platform, time_to_live):
+        pass
+
+    def custom_message(self):
+        pass
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="pyjpush",
+    version="0.1",
+    author="ax003d",
+    author_email="ax003d@gmail.com",
+    description="3rd party jpush server SDK.",
+    license="MIT License",
+    keywords="jpush sdk server",
+    url="http://github.com/ax003d/pyjpush",
+    )
