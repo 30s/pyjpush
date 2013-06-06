7da49dd6a6003e2122bc0d7457d4b22c1ab7a289
Robert <rzheng@xtremeprog.com>
2013-06-06 15:20:37 +0800

    * implement custom message

--- pyjpush/jpush.py
+++ pyjpush/jpush.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from hashlib import md5
 
 import requests
@@ -10,18 +12,24 @@ class JPushClient(object):
         self.app_key = app_key
         self.master_secret = master_secret
 
-    def sendmsg(self):
-        pass
+    def get_verification_code(self, sendno, receiver_type, receiver_value):
+        m  = md5()
+        m.update('%d%d%s%s' % (sendno, receiver_type, receiver_value, self.master_secret))
+        return m.hexdigest()
+
+    # def sendmsg(self, sendno, receiver_type, receiver_value, msg_type, msg_content, 
+    #             send_description, platform, time_to_live=86400):
+    #     vc = self.get_verification_code(sendno, receiver_type, receiver_value)
+        
 
     def notification(self, sendno, receiver_type, receiver_value, 
                      txt, platform, time_to_live=86400):
-        m  = md5()
-        m.update('%d%d%s%s' % (sendno, receiver_type, receiver_value, self.master_secret))
+        vc = self.get_verification_code(sendno, receiver_type, receiver_value)
         payload = {'sendno': sendno,
                    'app_key': self.app_key,
                    'receiver_type': receiver_type,
                    'receiver_value': receiver_value,
-                   'verification_code': m.hexdigest(),
+                   'verification_code': vc,
                    'txt': txt,
                    'platform': platform,
                    'time_to_live': time_to_live}
@@ -29,8 +37,19 @@ class JPushClient(object):
         return resp.json()
 
 
-    def custom_message(self):
-        pass
+    def custom_message(self, sendno, receiver_type, receiver_value, 
+                     txt, platform, time_to_live=86400):
+        vc = self.get_verification_code(sendno, receiver_type, receiver_value)
+        payload = {'sendno': sendno,
+                   'app_key': self.app_key,
+                   'receiver_type': receiver_type,
+                   'receiver_value': receiver_value,
+                   'verification_code': vc,
+                   'txt': txt,
+                   'platform': platform,
+                   'time_to_live': time_to_live}
+        resp = requests.post(JPushClient.URL + 'custom_message', data=payload)
+        return resp.json()
 
 
 if __name__=='__main__':
@@ -41,4 +60,4 @@ if __name__=='__main__':
     platform = 'android'
     jpc = JPushClient(app_key, master_secret)
     print jpc.notification(1, 1, imei, txt, platform)
-    
+    print jpc.custom_message(1, 1, imei, txt, platform)
