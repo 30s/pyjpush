93b0967f3ebab35fcdfefed55c74651ffb50aef3
Robert <rzheng@xtremeprog.com>
2013-06-06 12:26:16 +0800

    * implement notification api

--- pyjpush/jpush.py
+++ pyjpush/jpush.py
@@ -1,13 +1,32 @@
+from md5 import md5
+
+import requests
+
+
 class JPushClient(object):
-    def __init__(self):
-        pass
+    URL = 'http://api.jpush.cn:8800/sendmsg/v2/'
+
+    def __init__(self, app_key, master_secret):
+        self.app_key = app_key
+        self.master_secret = master_secret
 
     def sendmsg(self):
         pass
 
-    def notification(self, sendno, app_key, receiver_type, receiver_value, 
-                     txt, platform, time_to_live):
-        pass
+    def notification(self, sendno, receiver_type, receiver_value, 
+                     txt, platform, time_to_live=86400):
+        vc = md5('%d%d%s%s' % (sendno, receiver_type, receiver_value, self.master_secret)).digest()
+        payload = {'sendno': sendno,
+                   'app_key': self.app_key,
+                   'receiver_type': receiver_type,
+                   'receiver_value': receiver_value,
+                   'verification_code': vc,
+                   'txt': txt,
+                   'platform': platform,
+                   'time_to_live': time_to_live}
+        resp = requests.posts(JPushClient.URL + 'notification', data=payload)
+        return resp.json()
+
 
     def custom_message(self):
         pass
